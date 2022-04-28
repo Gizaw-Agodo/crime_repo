4b4fd9468c6c9da9453718265b243a295b13e9eb
Gizaw <gizawag123@gmail.com>
2022-04-28 12:11:15 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:crime_reporter/screens/login.dart';
+import 'package:crime_reporter/screens/signup.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      
+      home: SignUpPage(),
+      routes: {
+        '/login':(context)=>LoginPage(),
+        '/user/signup': (context)=>SignUpPage(),
+      },
+    );
+  }
+}
